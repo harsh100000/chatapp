d460ca03b8cb226fc1e01ae480d42b70502f5705
harsh_vardhan <harsh10112001@gmail.com>
2026-01-17 16:07:51 +0530

    completed app

--- Server/config/db.js
+++ Server/config/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const connectDB = async () =>{
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log("Connected to MongoDB ", conn.connection.host);
     } catch (error) {
         console.log(error.message);
     }
